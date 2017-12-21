2c839fe4b6f2622f47b0833c7ed2b4da3d14ca0b
JamesIsAwkward <34007678+JamesIsAwkward@users.noreply.github.com>
2017-12-20 20:38:26 -0600

    Added input buffer and lots of comments
    It will now save each keystroke in ram and then print it back on screen when the user presses enter.

--- main.asm
+++ main.asm
@@ -1,10 +1,19 @@
-;************************************
-;  SimpleShell                      *
-;  Super Simple Shell-like Monitor  *
-;  All it does for now is print     *
-;  the header data and then echo    *
-;  user input                       *
-;************************************
+;
+;  SimpleShell
+;  Super Simple Shell-like Monitor
+;  All it does for now is print
+;  the header data and then echo
+;  user input after pressing enter
+;
+
+; This will change as I go, especially when I add ROM
+; Future Memory Map
+;   ROM - 0000H - 7FFF
+;   RAM - 8000H - $FFFF
+;
+;   Input buffer - 3FFF - 401F ; 32 bytes
+
+
 
 ;******************************************************************
 ; Special thanks to MatthewWCook for his UART chart and code!!!!!
@@ -29,6 +38,8 @@
 ;******************************************************************
 
 ORG                 $0000
+
+; I'm using a ROM-less build for now, so this will be changed later
 RAMEND:     EQU     $7FFF       ; Top address of RAM
 
 ;******************************************************************
@@ -37,9 +48,9 @@ RAMEND:     EQU     $7FFF       ; Top address of RAM
 ;INIT_UART handles.... UART INIT
 ;******************************************************************
 INIT_SYSTEM:
-            LD     A,0
-            LD     B,0
-            LD     HL,0 
+            LD     A,0          ; Quick cleanup to help with testing
+            LD     B,0          ; I do frequent resets to check code
+            LD     HL,0         ;
             LD     SP,RAMEND    ; Loads the contents of RAMEND to Stack Pointer
             CALL   INIT_UART
             
@@ -47,8 +58,8 @@ INIT_SYSTEM:
 MAIN:
             CALL    CLEAR_SCREEN
             CALL    HEADER_START       ; This prints the header data on startup
-            ;HALT                 ; Halts the system - I use it to debug my hardware
-            JP      SHELLLOOP_PROMPT
+            ;HALT                      ; Halts the system - I used it to debug my hardware
+            JP      SHELLLOOP_START
 
 
 ;*******************************************************************
@@ -58,22 +69,64 @@ MAIN:
 ; At least.... it will :P
 ;*******************************************************************
 
+SHELLLOOP_START:
+            LD      HL,$3FFF            ; Manual memory assignment for the input buffer
+
 SHELLLOOP_PROMPT:
+            PUSH    HL
             LD      HL,SHELL_CURSORDATA
             CALL    UART_PRINTSTR
+            POP     HL
 
 SHELLLOOP:  
             CALL    USER_INPUT
-            CP      0DH                 ;0D is hex for carriage return AKA return button
-            JP      Z,SHELLLOOP_INPUT
-            CALL    UART_PRINT
-            JP      SHELLLOOP
+            CP      0DH                 ; 0D is hex for carriage return AKA return button
+            JP      Z,SHELLLOOP_INPUT_START ; If return is entered, go to shell loop input start function
+            CP      08H                 ; 08 is hex for backspace
+            CALL    Z,BACKSPACE         ; If backspace is input, then go to backspace function
+            CALL    INPUT_BUFFER        ; If not, call Input buffer function 
+            JP      SHELLLOOP           ; Start loop over
             
+BACKSPACE:                              ; Function dedicated to backspace.. this good practice?
+            LD      A,B                 ; Loads counter into A
+            CP      0                   ; Check if counter is 0
+            RET                         ; If it is, I don't want to backspace anymore to I ret
+            DEC     B                   ; If it isn't 0 then it will DEC because backspace
+            DEC     HL                  ; It'll also back up the input buffer as well
+            CALL    UART_PRINT          ; Print the backspace
+            RET                         ; Return
+            
+;
+; Currently has no overflow protection     
+; Just a test, this will echo back your input 
+; after you type it and press enter.      
+
+INPUT_BUFFER:
+            LD      (HL),A              ; Loads input data into HL pointer
+            INC     HL                  ; INC HL to get ready for next inputs
+            INC     B                   ; INC the counter to keep tabs on where the cursor is and how many bytes I've typed
+            CALL    UART_PRINT          ; Prints the input
+            RET                         ; Return
+           
+           
+           
+SHELLLOOP_INPUT_START:
+            LD      HL,$3FFF            ; Manually addressing the start of my input buffer
+           
 SHELLLOOP_INPUT:
-            CALL    NEW_LINE
-            JP      SHELLLOOP_PROMPT
+            LD      A,B                 ; Load the counter to A
+            CP      0                   ; See if the counter is at 0
+            JP      Z,SHELLLOOP_START   ; If so, there is no (or shouldn't be..) data to output
+            LD      A,(HL)              ; Loads A with data from buffer
+            CALL    UART_PRINT          ; Prints data
+            INC     HL                  ; Shifts HL to next byte for the next loop
+            DEC     B                   ; DEC counter to keep up with remaining data
+            JP      SHELLLOOP_INPUT     ; Start the loop over
             
-            
+SHELLLOOP_INPUT_END:
+            LD      B,0                 ; Reset the counter back to 0
+            CALL    NEW_LINE            ; Start a new blank line
+            JP      SHELLLOOP_START     ; 
 
 ;*******************************************************************
 ; Input functions
@@ -176,4 +229,6 @@ UART_RXCHECK:
 ;Data and Strings
 NEW_LINE_DATA:      DB      "\r",0
 SHELL_CURSORDATA:   DB      "\n\rSimpleShell>",0
-HEADERDATA:         DB      "\n\rSimpleShell v0.1.0\r\nCreated by: JamesIsAwkward\r",0
+HEADERDATA:         DB      "\n\rSimpleShell v0.1.1\r\nCreated by: JamesIsAwkward\r",0
+
+END
